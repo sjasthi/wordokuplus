aaf541d216cf8c996b4a2e190015ddea44bc4868
GreenestGlen <111922947+GreenestGlen@users.noreply.github.com>
2022-10-25 13:35:24 -0500

    Removed image bug
    Images are now deleted when new images are uploaded.

--- ajaxfile.php
+++ ajaxfile.php
@@ -1,6 +1,29 @@
 <?php
 session_start();
 $countFiles = count($_FILES['files']['name']);
+
+
+$dir = "uploads/" . session_id();
+$it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+$files = new RecursiveIteratorIterator($it,
+             RecursiveIteratorIterator::CHILD_FIRST);
+foreach($files as $file) {
+    if ($file->isDir()){
+        rmdir($file->getRealPath());
+    } else {
+        unlink($file->getRealPath());
+    }
+}
+//rmdir($dir);
+
+
+
+rmdir("uploads/" . session_id());
+
+
+
+
+
 mkdir("uploads/" . session_id(), 0777);
 $upload_location = "uploads/" . session_id() . "/";
 
